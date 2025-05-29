dac90b789a88c6bcd123a3e1c7c53d1ceb6c6253
Hanna Pęciak <103579210+haszpe@users.noreply.github.com>
2025-05-29 19:55:00 +0200

    Add filtering

---
+++ vcf_filter.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+REF="/mnt/ngs-analysis/Data/ref_data/Homo_sapiens.GRCh38.dna.primary_assembly.fa"
+
+FOLDER_GATK="Data/GatkVariantCalling"
+FOLDER_BCF="Data/BcfVariantCalling"
+
+FILES=( "$FOLDER_GATK"/*_variants.vcf "$FOLDER_BCF"/*_variants.vcf )
+
+for VCF in "${FILES[@]}"; do
+  BASENAME=$(basename "$VCF" .vcf)
+  DIRNAME=$(dirname "$VCF")
+
+  FILTERED_VCF="${DIRNAME}/${BASENAME}_filtered.vcf"
+  CLEAN_VCF="${DIRNAME}/${BASENAME}_clean.vcf"
+
+  echo "Processing: $VCF"
+
+  gatk VariantFiltration \
+    -R "$REF" \
+    -V "$VCF" \
+    --filter-expression "QUAL < 30.0" \
+    --filter-name "LowQual" \
+    --filter-expression "DP > 500" \
+    --filter-name "HighDepth" \
+    --filter-expression "vc.getNAlleles() > 2" \
+    --filter-name "MultiAllelic" \
+    -O "$FILTERED_VCF"
+
+  gatk SelectVariants \
+    -R "$REF" \
+    -V "$FILTERED_VCF" \
+    --exclude-filtered \
+    -O "$CLEAN_VCF"
+
+  echo "Finished filtering: $VCF → $CLEAN_VCF"
+  echo "------------------------------------------------------"
+done
